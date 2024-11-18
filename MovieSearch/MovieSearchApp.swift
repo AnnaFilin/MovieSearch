cb54536875e6745ab83c6ed0d2f506ccb6b84909
Anna Filin <68apple@gmail.com>
2024-11-18 12:11:21 +0200

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by Anna Filin on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
